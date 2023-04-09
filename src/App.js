ce60b8bf297da899a6569c336a16c34bbef9ec7b
ahsn.dev <ahsn.dev@gmail.com>
2023-04-09 07:38:37 +0330

    add cart section

--- src/App.js
+++ src/App.js
@@ -1,10 +1,10 @@
 import El from "./library/El";
-import login from "./pages/login";
+import cart from "./pages/cart";
 
 const App = () => {
   const app = El({
     element: "div",
-    child: [login()],
+    child: [cart()],
   });
 
   return app;
